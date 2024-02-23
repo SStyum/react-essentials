6ee1b327161bff51148ece243c0ffff93def4001
Guilherme Rocha <guirocha1938@gmail.com>
2024-02-22 21:41:14 -0300

    feat: Adds state management for dynamic content on tab buttons section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,48 @@
+import { useState } from 'react';
+
 import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 import Header from "./components/Header";
 import componentsImg from './assets/components.png';
 import propsImg from './assets/config.png';
 import jsxImg from './assets/jsx-ui.png';
 import stateImg from './assets/state-mgmt.png';
 
-export const CORE_CONCEPTS = [
-  {
-    image: componentsImg,
-    title: 'Components',
-    description:
-      'The core UI building block - compose the user interface by combining multiple components.',
-  },
-  {
-    image: jsxImg,
-    title: 'JSX',
-    description:
-      'Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.',
-  },
-  {
-    image: propsImg,
-    title: 'Props',
-    description:
-      'Make components configurable (and therefore reusable) by passing input data to them.',
-  },
-  {
-    image: stateImg,
-    title: 'State',
-    description:
-      'React-managed data which, when changed, causes the component to re-render & the UI to update.',
-  },
-];
-
 function App() {
+
+  const CORE_CONCEPTS = [
+    {
+      image: componentsImg,
+      title: 'Components',
+      description:
+        'The core UI building block - compose the user interface by combining multiple components.',
+    },
+    {
+      image: jsxImg,
+      title: 'JSX',
+      description:
+        'Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.',
+    },
+    {
+      image: propsImg,
+      title: 'Props',
+      description:
+        'Make components configurable (and therefore reusable) by passing input data to them.',
+    },
+    {
+      image: stateImg,
+      title: 'State',
+      description:
+        'React-managed data which, when changed, causes the component to re-render & the UI to update.',
+    },
+  ];
+
+  const handleSelect = (selectedButton) => {
+    setTabContent(selectedButton);
+  };
+
+  const [tabContent, setTabContent] = useState('Please select a concept to view.')
+
   return (
     <div>
       <Header />
@@ -46,6 +56,16 @@ function App() {
             )
           })}
         </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {tabContent}
         </section>
       </main>
     </div>
